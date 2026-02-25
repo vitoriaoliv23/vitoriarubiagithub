eaae3a52ff871949ec7a8f75fb629483ddc6e91c
vitoriaoliv23 <cromebookescola04@gmail.com>
2026-02-25 09:48:17 -0300

    Create 01.js

---
+++ back_end/01.js
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>programa que retorna o valor absoluto de um número</title>
+</head>
+Autor: vitoria rubia 
+<body>
+  <script>
+    var valor1 = Math.abs (-1234);
+    alert (valor1);
+  </script>
+</body>
+</html>
