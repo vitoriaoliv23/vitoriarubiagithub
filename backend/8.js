6b2bd7a5653fa9bcae97a69e509d4e6074f3110d
vitoriaoliv23 <cromebookescola04@gmail.com>
2026-02-27 16:09:51 -0300

    Add HTML file with JavaScript alerts for floor values

---
+++ backend/8.js
@@ -0,0 +1,14 @@
+<html>
+<head>
+<title>...</title>
+</head>
+
+<body>
+  <script>
+    var valor1 = Math.floor (100.25);
+    var valor2 = Math.floor (-100.25);
+    alert (valor1);
+    alert (valor2);
+  </script>
+</body>
+</html>
