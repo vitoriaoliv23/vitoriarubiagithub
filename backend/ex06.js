5d7dd7941aff7cd9d19cf9d6bbc9a353f7a630ef
vitoriaoliv23 <cromebookescola04@gmail.com>
2026-02-27 16:01:37 -0300

    Create ex06.js

---
+++ backend/ex06.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Este método retornará o cosseno (em radianos) de um número.</title>
+</head>
+Autor: vitoria
+
+<body>
+  <script>
+    var valor1 = Math.cos (0.14);
+    alert (valor1);
+  </script>
+</body>
+</html>
