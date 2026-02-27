0de214d4f4909afad268ef396a59ad22e0e27689
vitoriaoliv23 <cromebookescola04@gmail.com>
2026-02-27 16:11:56 -0300

    Add HTML file with power calculation script

---
+++ backend/12.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Este método retorna a base elevada à potência do expoente. Por exemplo, 2 elevado a décima potência é 1024.</title>
+</head>
+  Autr: Vitoria
+
+<body>
+  <script>
+    var valor1 = Math.pow ( 1024,2);
+    alert (valor1);
+  </script>
+</body>
+</html>
