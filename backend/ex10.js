7714017cd0d7f6c57050af04bdbddab8f210e17b
vitoriaoliv23 <cromebookescola04@gmail.com>
2026-02-27 16:04:48 -0300

    Add HTML file to demonstrate Math.max usage

---
+++ backend/ex10.js
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Este método retorna o maior valor entre dois números.
+
+<html></title>
+</head>
+Autor: vitoria
+
+<body>
+  <script>
+    var valor1 = Math.max ( 3,9);
+    alert (valor1);
+  </script>
+</body>
+</html>
