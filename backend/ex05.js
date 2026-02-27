19128ad6c79eaa3bdbea292953877ef81573e366
vitoriaoliv23 <cromebookescola04@gmail.com>
2026-02-27 16:00:22 -0300

    Create ex05.js with Math.ceil example
    Add HTML file with a script that uses Math.ceil

---
+++ backend/ex05.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Este método retorna um inteiro maior ou igual a um número</title>
+</head>
+autor: vitoria
+  
+<body>
+  <script>
+    var valor1 = Math.ceil (-14.6);
+    alert (valor1);
+  </script>
+</body>
+</html>
