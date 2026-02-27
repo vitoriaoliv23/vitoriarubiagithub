a6efe928fe7d13c681d91152cb1aae1f3fedebfe
vitoriaoliv23 <cromebookescola04@gmail.com>
2026-02-27 16:05:19 -0300

    Add HTML file to demonstrate Math.min usage

---
+++ backend/ex11.js
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title>Este método retorna o menor valor entre dois números.
+
+<html></title>
+</head>
+  Autor: Yasmim
+
+<body>
+  <script>
+    var valor1 = Math.min ( 3,9);
+    alert (valor1);
+  </script>
+</body>
+</html>
+
+                           
