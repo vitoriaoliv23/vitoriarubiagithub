e56bba5e9f9733391c7c6b5dd5683f220c6fe01b
vitoriaoliv23 <cromebookescola04@gmail.com>
2026-02-27 16:03:21 -0300

    Create ex08.js to calculate Euler's constant
    Add HTML file to demonstrate Euler's constant calculation.

---
+++ backend/ex08.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Este método retornará o valor da constante de Euler elevada ao número informado, ou seja, E elevado ao parâmetro.</title>
+</head>
+Autor: vitoria
+
+<body>
+  <script>
+    var valor1 = Math.exp (0.0007);
+    alert (valor1);
+  </script>
+</body>
+</html>
