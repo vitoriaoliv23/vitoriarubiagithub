a066199d7b552313b5686a7cf932996e6a8c99d4
vitoriaoliv23 <cromebookescola04@gmail.com>
2026-02-27 15:58:30 -0300

    Add HTML file to calculate arccosine of a number

---
+++ backend/ex02.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>retornará o arco cosseno (em radianos) de um número.</title>
+</head>
+Autor: vitoria
+
+<body>
+  <script>
+    var valor1 = Math.acos (0.14);
+    alert (valor1);
+  </script>
+</body>
+</html>
