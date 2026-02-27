4ca6037852d65bf471bbffb73b6a18ad9160ab2f
vitoriaoliv23 <cromebookescola04@gmail.com>
2026-02-27 15:59:04 -0300

    Add HTML file to calculate arcsine of a value

---
+++ backend/ex03.js
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>Este método retorna o arco seno (em radianos) de um valor.</title>
+</head>
+Autor: vitoria
+  
+<body>
+  <script>
+    var valor1 = Math.asin (0.14);
+    alert (valor1);
+  </script>
+</body>
+</html>
